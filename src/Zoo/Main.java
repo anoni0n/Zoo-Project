ca812b449adbf6e722be2ab3c1838e1f707494f5
anoni0n <ananya.i@icloud.com>
2024-09-25 14:47:27 -0700

    Zoo Project parts 1 & 2(in progress)

--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -7,20 +7,25 @@ import java.util.Scanner;
 
 public class Main
 {
-
-    public static void main(String[] args){
-        File camel = new File("src/Zoo/camel.txt");
-        String animalType = "Camel";
-        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
-        Double habitatTemperature = Math.random()*(100-50)+50;
-        System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %2fF",animalType,(currentTime.getHour()>12) ? currentTime.minusHours(12)+"PM":currentTime+"AM",habitatTemperature);
-
+    public static void printAnimal(File f, String a){
         try {
-            Scanner scanner = new Scanner(camel);
+            Scanner scanner = new Scanner(f);
             while (scanner.hasNextLine()){
                 String i = scanner.nextLine();
                 System.out.println(i);}
-       } catch (Exception e) {System.out.println(e);}
+        } catch (Exception e) {System.out.printf("There is no animal \"%s\" in the zoo.",a);}
+    }
+    public static void main(String[] args){
 
+        System.out.println("Which animal would you like to view?");
+        Scanner userScanner = new Scanner(System.in);
+        String animalType = userScanner.nextLine();
+        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+        Double habitatTemperature = Math.random()*(100-50)+50;
+
+        System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %2fF%n%n",animalType,(currentTime.getHour()>12) ? currentTime.minusHours(12)+"PM":currentTime+"AM",habitatTemperature);
+
+        File animal = new File("src/Zoo/"+animalType.toLowerCase()+".txt");
+        printAnimal(animal, animalType);
     }
 }
