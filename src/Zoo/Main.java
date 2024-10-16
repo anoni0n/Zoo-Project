8ce86ba9fbbe6f82e2883dfe6e686851bd66ac82
anoni0n <ananya.i@icloud.com>
2024-10-15 17:44:12 -0700

    Zoo Project part 3 (final copy - no personal additions)

--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void printAnimal(File f, String a){
+    public static void printAnimal(String a){
+        File animalFile = new File("src/Zoo/"+a+".txt");
+        System.out.println("You are now viewing the "+a+" habitat.\n");
         try {
-            Scanner scanner = new Scanner(f);
+            Scanner scanner = new Scanner(animalFile);
             while (scanner.hasNextLine()){
                 String i = scanner.nextLine();
                 System.out.println(i);}
@@ -18,20 +20,43 @@ public class Main
     }
     public static void main(String[] args){
 
-        System.out.printf("Please enter the name of the animal you would like to view:%n" +
-                "%nCamel%nElephant%nGiraffe%nGorilla%nLion%nMonkey%nPanda%nPenguin%nTiger%nTurtle%nZebra%n%n");
+        System.out.printf("Please enter the number of the habitat you would like to view:%n" +
+                "%n1 - Camel%n2 - Elephant%n3 - Giraffe%n4 - Gorilla%n5 - Lion%n6 - Monkey%n7 - Panda%n8 - Penguin%n9 - Tiger%n10 - Turtle%n11 - Zebra%n%n");
         Scanner userScanner = new Scanner(System.in);
-        String animalType = userScanner.nextLine();
+        int animalNum = userScanner.nextInt();
         LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
         Double habitatTemperature = Math.random()*(100-50)+50;
 
-        File animal = new File("src/Zoo/"+animalType.toLowerCase()+".txt");
-        if (!animal.exists()){
-            System.out.printf("There is no animal \"%s\" in the zoo.",animalType);
+        String animalType;
+        switch (animalNum){
+            case 1: animalType = "camel";
+            break;
+            case 2: animalType = "elephant";
+            break;
+            case 3: animalType = "giraffe";
+            break;
+            case 4: animalType = "gorilla";
+            break;
+            case 5: animalType = "lion";
+            break;
+            case 6: animalType = "monkey";
+            break;
+            case 7: animalType = "panda";
+            break;
+            case 8: animalType = "penguin";
+            break;
+            case 9: animalType = "tiger";
+            break;
+            case 10: animalType = "turtle";
+            break;
+            case 11: animalType = "zebra";
+            break;
+            default: animalType = null;
+            System.out.println("Chosen camera does not exist");
             System.exit(0);
+            break;
         }
-
         System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n",animalType,(currentTime.getHour()>12) ? currentTime.minusHours(12)+"PM":currentTime+"AM",habitatTemperature);
-        printAnimal(animal, animalType);
+        printAnimal(animalType);
     }
 }
