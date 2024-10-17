8c3869a9a5b9c98635718b32f8e0e84ed725e10e
anoni0n <ananya.i@icloud.com>
2024-10-17 15:01:21 -0700

    Zoo Project part 3 (final copy - no personal additions)

--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -14,24 +14,30 @@ public class Main
                 String i = scanner.nextLine();
                 System.out.println(i);}
         } catch (Exception e) {System.out.printf("There is no animal \"%s\" in the zoo.",a);}
-        System.out.println("You've reached the end of the program. To check another habitat, please restart the watcher.");
     }
     public static void main(String[] args){
 
-        System.out.printf("Please enter the name of the animal you would like to view:%n" +
-                "%nCamel%nElephant%nGiraffe%nGorilla%nLion%nMonkey%nPanda%nPenguin%nTiger%nTurtle%nZebra%n%n");
-        Scanner userScanner = new Scanner(System.in);
-        String animalType = userScanner.nextLine();
-        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
-        Double habitatTemperature = Math.random()*(100-50)+50;
+        int userIntent = 1;
+        while (userIntent == 1) {
+            System.out.printf("Please enter the name of the animal you would like to view:%n" +
+                    "%nCamel%nElephant%nGiraffe%nGorilla%nLion%nMonkey%nPanda%nPenguin%nTiger%nTurtle%nZebra%n%n");
+            Scanner userScanner = new Scanner(System.in);
+            String animalType = userScanner.nextLine();
+            LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+            Double habitatTemperature = Math.random() * (100 - 50) + 50;
 
-        File animal = new File("src/Zoo/"+animalType.toLowerCase()+".txt");
-        if (!animal.exists()){
-            System.out.printf("There is no animal \"%s\" in the zoo.",animalType);
-            System.exit(0);
+            File animal = new File("src/Zoo/" + animalType.toLowerCase() + ".txt");
+            if (!animal.exists()) {
+                System.out.printf("There is no animal \"%s\" in the zoo.", animalType);
+                System.exit(0);
+            }
+
+            System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n", animalType, (currentTime.getHour() > 12) ? currentTime.minusHours(12) + "PM" : currentTime + "AM", habitatTemperature);
+            printAnimal(animal, animalType);
+
+            System.out.println("\nYou've reached the end of the program. To check another habitat, enter \"1\" to exit the program, enter \"0\"");
+            userIntent = userScanner.nextInt();
         }
-
-        System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n",animalType,(currentTime.getHour()>12) ? currentTime.minusHours(12)+"PM":currentTime+"AM",habitatTemperature);
-        printAnimal(animal, animalType);
+        System.exit(0);
     }
 }
