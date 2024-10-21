765302c13115105891b528c629a92287ee2b354b
anoni0n <ananya.i@icloud.com>
2024-10-21 14:03:53 -0700

    Zoo Project part 4

--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -18,45 +18,66 @@ public class Main
         } catch (Exception e) {System.out.printf("There is no animal \"%s\" in the zoo.",a);}
         System.out.println("You've reached the end of the program. To check another habitat, please restart the watcher.");
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.printf("Please enter the number of the habitat you would like to view:%n" +
-                "%n1 - Camel%n2 - Elephant%n3 - Giraffe%n4 - Gorilla%n5 - Lion%n6 - Monkey%n7 - Panda%n8 - Penguin%n9 - Tiger%n10 - Turtle%n11 - Zebra%n%n");
-        Scanner userScanner = new Scanner(System.in);
-        int animalNum = userScanner.nextInt();
-        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
-        Double habitatTemperature = Math.random()*(100-50)+50;
+        int userIntent = 1;
+        while (userIntent == 1) {
+            System.out.printf("Please enter the number of the habitat you would like to view:%n" +
+                    "%n1 - Camel%n2 - Elephant%n3 - Giraffe%n4 - Gorilla%n5 - Lion%n6 - Monkey%n7 - Panda%n8 - Penguin%n9 - Tiger%n10 - Turtle%n11 - Zebra%n%n");
+            Scanner userScanner = new Scanner(System.in);
+            int animalNum = userScanner.nextInt();
+            LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+            Double habitatTemperature = Math.random() * (100 - 50) + 50;
 
-        String animalType;
-        switch (animalNum){
-            case 1: animalType = "Camel";
-            break;
-            case 2: animalType = "Elephant";
-            break;
-            case 3: animalType = "Giraffe";
-            break;
-            case 4: animalType = "Gorilla";
-            break;
-            case 5: animalType = "Lion";
-            break;
-            case 6: animalType = "Monkey";
-            break;
-            case 7: animalType = "Panda";
-            break;
-            case 8: animalType = "Penguin";
-            break;
-            case 9: animalType = "Tiger";
-            break;
-            case 10: animalType = "Turtle";
-            break;
-            case 11: animalType = "Zebra";
-            break;
-            default: animalType = null;
-            System.out.println("Chosen camera does not exist");
-            System.exit(0);
-            break;
+            String animalType;
+            switch (animalNum) {
+                case 1:
+                    animalType = "Camel";
+                    break;
+                case 2:
+                    animalType = "Elephant";
+                    break;
+                case 3:
+                    animalType = "Giraffe";
+                    break;
+                case 4:
+                    animalType = "Gorilla";
+                    break;
+                case 5:
+                    animalType = "Lion";
+                    break;
+                case 6:
+                    animalType = "Monkey";
+                    break;
+                case 7:
+                    animalType = "Panda";
+                    break;
+                case 8:
+                    animalType = "Penguin";
+                    break;
+                case 9:
+                    animalType = "Tiger";
+                    break;
+                case 10:
+                    animalType = "Turtle";
+                    break;
+                case 11:
+                    animalType = "Zebra";
+                    break;
+                default:
+                    animalType = null;
+                    System.out.println("Chosen camera does not exist.");
+                    break;
+            }
+
+            if (animalType != null) {
+                System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n", animalType, (currentTime.getHour() > 12) ? currentTime.minusHours(12) + "PM" : currentTime + "AM", habitatTemperature);
+                printAnimal(animalType);
+            }
+            //asks the user in they want to continue the program and loops if they do
+            System.out.println("\nTo check another habitat, enter \"1\" to exit the program, enter \"0\"");
+            userIntent = userScanner.nextInt();
         }
-        System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n",animalType,(currentTime.getHour()>12) ? currentTime.minusHours(12)+"PM":currentTime+"AM",habitatTemperature);
-        printAnimal(animalType);
+        System.exit(0);
     }
 }
