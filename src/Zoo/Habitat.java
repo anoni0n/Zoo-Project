989f49af4098e2d4887ea5092a0bce3957b0d681
anoni0n <ananya.i@icloud.com>
2025-02-18 13:49:09 -0800

    added javadocs and comments

--- src/Zoo/Habitat.java
+++ src/Zoo/Habitat.java
@@ -5,25 +5,43 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class Habitat {
-
-    private static ArrayList<Habitat> habitats = new ArrayList<>();
+    //static ArrayList that stores all habitats
+    static ArrayList<Habitat> habitats = new ArrayList<>();
+    private final double habitatTemperature = Math.random() * 50 + 50;
     private final String animalType;
-    private final double habitatTemperature;
 
-    public Habitat(String animalType, double habitatTemperature){
-        this.animalType = animalType;
-        this.habitatTemperature = habitatTemperature;
+    public Habitat(String animalType){
+        //capitalizes the first character of animalType
+        this.animalType = animalType.replaceFirst(animalType.substring(0,1),animalType.substring(0,1).toUpperCase());
         habitats.add(this);
     }
 
+    /**
+     * prints the ArrayList habitats
+     */
     public static void printHabitats(){
-        System.out.println(habitats);
+        System.out.println("Cameras: "+habitats);
+    }
+
+    /**
+     * returns the type of animal the habitat is for
+     * @return animalType
+     */
+    public String getAnimalType(){
+        return animalType;
+    }
+
+    /**
+     * prints all required information on a habitat
+     */
+    public void printInfo(){
+        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES) ;
+        System.out.printf("Animal Type: %s%nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n",
+                animalType, (currentTime.getHour() > 12) ? currentTime.minusHours(12) + "PM" : currentTime + "AM", habitatTemperature);
     }
 
     @Override
     public String toString() {
-        LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES) ;
-        return String.format("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n",
-                animalType, (currentTime.getHour() > 12) ? currentTime.minusHours(12) + "PM" : currentTime + "AM", habitatTemperature);
+        return animalType.toUpperCase();
     }
 }
--- src/Zoo/Main.java
+++ src/Zoo/Main.java
@@ -1,45 +1,84 @@
 package Zoo;
 
-import java.time.LocalTime;
 import java.io.File;
-import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+import static Zoo.Habitat.printHabitats;
+import static Zoo.Habitat.habitats;
 
 public class Main
 {
-    public static void printAnimal(File f, String a){
-        try {
-            Scanner scanner = new Scanner(f);
-            while (scanner.hasNextLine()){
-                String i = scanner.nextLine();
-                System.out.println(i);}
-        } catch (Exception e) {System.out.printf("There is no animal \"%s\" in the zoo.",a);}
-    }
     public static void main(String[] args){
 
+        //makes all the starting Habitats
+        Habitat camel = new Habitat("Camel");
+        Habitat elephant = new Habitat("Elephant");
+        Habitat giraffe = new Habitat("giraffe");
+        Habitat gorilla = new Habitat("gorilla");
+        Habitat lion = new Habitat("lion");
+        Habitat monkey = new Habitat("monkey");
+        Habitat panda = new Habitat("panda");
+        Habitat penguin = new Habitat("penguin");
+        Habitat turtle = new Habitat("turtle");
+        Habitat zebra = new Habitat("zebra");
+
+        //repeats the program while the user still wishes to
         int userIntent = 1;
         while (userIntent == 1) {
-            System.out.printf("Please enter the name of the animal you would like to view:%n" +
-                    "%nCamel%nElephant%nGiraffe%nGorilla%nLion%nMonkey%nPanda%nPenguin%nTiger%nTurtle%nZebra%n%n");
             Scanner userScanner = new Scanner(System.in);
-            String animalType = userScanner.nextLine();
-            LocalTime currentTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
-            Double habitatTemperature = Math.random() * 50 + 50;
-
-            //creates a file object and checks if such an animal exists in the zoo
-            File animal = new File("src/Zoo/animals/"+animalType+".txt");
-            if (!animal.exists()){
-                System.out.printf("There is no animal \"%s\" in the zoo.", animalType);
+            System.out.println("To check a habitat, enter 1. To make a new habitat, enter 0.");
+            if (userScanner.nextInt() == 1) {
+                printHabitats();
+                System.out.printf("Please enter the name of the habitat you would like to view:%n");
+                String animalType = userScanner.next();
+                printCameraFeed(animalType);
             }
             else {
-                //displays the animal's information
-                System.out.printf("Animal Type: %s %nCurrent Time: %s%nHabitat Temperature: %.2fF%n%n", animalType, (currentTime.getHour() > 12) ? currentTime.minusHours(12) + "PM" : currentTime + "AM", habitatTemperature);
-                printAnimal(animal, animalType);
+                System.out.println("Enter the name of the new habitat:");
+                boolean valid = false;
+                while (!valid) {
+                    String newAnimal = userScanner.next();
+                    for (int i = 0; i < habitats.size(); i++) {
+                        if (habitats.get(i).getAnimalType().equalsIgnoreCase(newAnimal)) {
+                            System.out.println("That habitat already exists. Enter a different name:");
+                            break;
+                        }
+                        else if (i == habitats.size()-1){
+                            new Habitat(newAnimal);
+                            valid = true;
+                            break;
+                        }
+                    }
+                }
             }
-                //asks the user in they want to continue the program and loops if they do
-                System.out.println("\nTo check another habitat, enter \"1\" to exit the program, enter \"0\"");
-                userIntent = userScanner.nextInt();
+            //asks the user in they want to continue the program and loops if they do
+            System.out.println("To check another habitat or make a new habitat, enter 1. To exit the program, enter 0.");
+            userIntent = userScanner.nextInt();
         }
-        System.exit(0);
+    }
+
+    /**
+     * checks if the animal habitat exists and prints its information if it does
+     * @param animalHabitat the habitat to be printed
+     */
+    public static void printCameraFeed(String animalHabitat){
+        //finds the requested animal in habitats and prints its information if it exists
+        for (int i = 0; i < habitats.size(); i++){
+            if (habitats.get(i).getAnimalType().equalsIgnoreCase(animalHabitat)){
+                habitats.get(i).printInfo();
+                File animalImage = new  File("src/Zoo/animals/"+animalHabitat.toLowerCase()+".txt");
+                try {
+                    Scanner scanner = new Scanner(animalImage);
+                    while (scanner.hasNextLine()){
+                        String j = scanner.nextLine();
+                        System.out.println(j);}
+                } //catches if there is no ASCII of the animal to be printed
+                catch (Exception e) {System.out.println("[camera not found]"); }
+                break;
+            }
+            //prints if the animal requested doesn't exist in habitats
+            else if (i == habitats.size()-1){
+                System.out.println("There is no animal \""+animalHabitat+"\" in the zoo.");
+            }
+        }
     }
 }
